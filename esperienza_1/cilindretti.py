09a906105b6c25fc9f22f26e1ecfc70df15b84db
Francesco Pasa <francescopasa@gmail.com>
2013-03-13 09:29:36 +0100

    Updated plots

--- esperienza_1/cilindretti.py
+++ esperienza_1/cilindretti.py
@@ -3,26 +3,59 @@
 from math import *
 import csv
 import itertools
-import matplotlib.pyplot as plt
-from matplotlib import rc
+
+mpl = False
+try:
+    import matplotlib.pyplot as plt
+    from matplotlib import rc
+    mpl = True
+except:
+    pass
 
 def mean(data):
     return sum(data) / len(data)
 
+tex = "\\begin{table}\n\t\\begin{tabular} "
+
 # leggi i dati
 header = []
 meas = {}
+raw_meas = {}
 with open('cilindretti/misure_cilindretti.csv') as csvfile:
     data = csv.reader(csvfile)
 
+    l = 0
     for n, row in enumerate(data):
+        l += 1
+
         if n == 0:
             for i, name in enumerate(row):
                 header.append(name.strip())
                 meas[name.strip()] = []
+                raw_meas[name.strip()] = []
         else:
             for i, value in enumerate(row):
                 meas[header[i]].append(float(value))
+                raw_meas[header[i]].append(value.strip())
+
+    tex += "{{ {} }}\n".format(" | ".join(["c c c c c"] * 3))
+
+    tex += "\t\t\\toprule\n\t\t\multicolumn{5}{c}{Metro [mm]} & \multicolumn{5}{c}{Calibro [mm]} & \multicolumn{5}{c}{Micrometro [mm]} \\\\\n"
+    tex += "\t\t\midrule\n"
+    
+    for i in range(5):
+        tex += "\t\t" + " & ".join([x for n, x in enumerate(raw_meas["METRO"]) if ((n - i) % 5) == 0])
+
+        tex += " & " + " & ".join([x for n, x in enumerate(raw_meas["CALIBRO"]) if ((n - i) % 5) == 0])
+
+        tex += " & " + " & ".join([x for n, x in enumerate(raw_meas["MICROMETRO"]) if ((n - i) % 5) == 0])
+
+        tex += " \\\\\n"
+
+tex += "\t\\bottomrule\n\t\\end{tabular}\n\\end{table}"
+
+print tex
+print
 
 # compute some stuff
 # Metro
@@ -58,61 +91,107 @@ x_min_metro = 12.5
 x_max_metro = 14.5
 bins_metro = [x_min_metro + i * delta_metro for i in range(int((x_max_metro - x_min_metro) / delta_metro) + 1)]
 
-# bins (calibro micrometro -> cm)
+# bins (Calibro Micrometro -> cm)
 delta_cm = 0.05
 x_min_cm = 13.675
 x_max_cm = 13.975
 bins_cm = [x_min_cm + i * delta_cm for i in range(int(round((x_max_cm - x_min_cm) / delta_cm)) + 1)]
 
-f1 = plt.figure()
-f2 = plt.figure(figsize=(6, 8))
+if mpl:
+    # create 2 figures
+    f1 = plt.figure()
+    f2 = plt.figure(figsize=(6.5, 8.5))
 
-ax = f1.add_subplot(1, 1, 1)
-n_metro, r_bins_metro, patches_metro = ax.hist(xs_metro, bins_metro,
-    normed=True, color=(0, 0.65, 1), alpha=0.7)
 
-prob = ax.twinx()
-prob.set_ylabel(ur"Frequenza campionaria", fontsize=14)
 
-ax.locator_params(axis='x', nbins=4)
+    # plot figure 1
+    ax = f1.add_subplot(1, 1, 1)
+    n_metro, r_bins_metro, patches_metro = ax.hist(xs_metro, bins_metro,
+        normed=True, color=(0, 0.65, 1), alpha=0.7)
 
-ax.set_xlim(left=12, right=15)
-t = ax.set_title("Metro a nastro", fontsize=15)
-t.set_y(1.05)
-ax.set_xlabel(u'Lunghezza [mm]', fontsize=14)
-ax.set_ylabel(ur'Densità campionaria $[\frac{1}{mm}]$', fontsize=14)
-ax.grid(True)
+    # add another axis
+    prob = ax.twinx()
+    prob.set_ylabel(ur"Frequenza campionaria", fontsize=14)
 
-f1.suptitle('Lunghezza dei cilindretti', y=0.95, fontsize=16)
-f1.subplots_adjust(left=0.12, right=0.88, top=0.8, bottom=0.15, wspace=0.35)
+    ax.locator_params(axis='x', nbins=4)
 
-# istogramma
-ax1 = f2.add_subplot(2, 1, 1)
-ax2 = f2.add_subplot(2, 1, 2)
+    # properties of first graph
+    ax.set_xlim(left=12, right=15)
+    t = ax.set_title("Metro a nastro", fontsize=15)
+    t.set_y(1.05)
+    ax.set_xlabel(u'Lunghezza [mm]', fontsize=14)
+    ax.set_ylabel(ur'Densità campionaria $[\frac{1}{mm}]$', fontsize=14)
+    ax.set_xticks([13, 14])
+    ax.grid(True)
 
-n_calibro, r_bins_calibro, patches_calibro = ax1.hist(xs_calibro, bins_cm,
-    normed=True, color=(0, 0.65, 1), alpha=0.5)
+    # layout of first figure
+    f1.suptitle('Lunghezza dei cilindretti', y=0.95, fontsize=16)
+    f1.subplots_adjust(left=0.12, right=0.88, top=0.8, bottom=0.15, wspace=0.35)
 
-ax1.set_xlim(left=13.65, right=14)
-ax1.set_ylim(bottom=0, top=8)
-t = ax1.set_title("Calibro", fontsize=15)
-t.set_y(1.05)
-ax1.set_xlabel(u'Lunghezza [mm]')
-ax1.set_ylabel(ur'Densità campionaria')
-ax1.grid(True)
 
-n_micro, r_bins_micro, patches_micro = ax2.hist(xs_micro, bins_cm,
-    normed=True, color=(0.4, 0.8, 0), alpha=0.5)
 
-ax2.set_xlim(left=13.65, right=14)
-ax2.set_ylim(bottom=0, top=8)
-t = ax2.set_title("Micrometro", fontsize=15)
-t.set_y(1.05)
-ax2.set_xlabel(u'Lunghezza [mm]')
-ax2.set_ylabel(u'Densità campionaria')
-ax2.grid(True)
+    # Second figure. We add 2 subplots.
+    ax1 = f2.add_subplot(2, 1, 1)
+    ax2 = f2.add_subplot(2, 1, 2)
 
-f2.suptitle('Lunghezza dei cilindretti', y=0.95, fontsize=16)
-f2.subplots_adjust(left=0.11, right=0.89, top=0.86, bottom=0.1, hspace=0.4)
 
-plt.show()
+    # add second axis to first subplot
+    prob1 = ax1.twinx()
+    prob1.set_ylim((0, 0.5))
+    prob1.set_ylabel(ur"Frequenza campionaria")
+
+    # first subplot: calibro
+    n_calibro, r_bins_calibro, patches_calibro = ax1.hist(xs_calibro, bins_cm,
+        normed=True, color=(0, 0.65, 1), alpha=0.5)
+    
+    # properties of calibro subplot
+    ax1.set_xlim(left=13.65, right=14)
+    ax1.set_ylim(bottom=0, top=10)
+    t = ax1.set_title("Calibro", fontsize=15)
+    t.set_y(1.05)
+    ax1.set_xlabel(u'Lunghezza [mm]')
+    ax1.set_ylabel(ur'Densità campionaria $[\frac{1}{mm}]$')
+
+    ticks = [13.70, 13.75, 13.80, 13.85, 13.90, 13.95]
+    ax1.set_xticks(ticks)
+
+    for label in ax1.get_xticklabels():
+        label.set_rotation(30)
+
+    ax1.grid(True)
+
+
+    # add second axis to second subplot
+    prob2 = ax2.twinx()
+    prob2.set_ylim((0, 0.5))
+    prob2.set_ylabel(ur"Frequenza campionaria")
+
+    # second subplot: micrometro
+    n_micro, r_bins_micro, patches_micro = ax2.hist(xs_micro, bins_cm,
+        normed=True, color=(0.4, 0.8, 0), alpha=0.5)
+    
+    # properties of micrometro subplot
+    ax2.set_xlim(left=13.65, right=14)
+    ax2.set_ylim(bottom=0, top=10)
+    t = ax2.set_title("Micrometro", fontsize=15)
+    t.set_y(1.05)
+    ax2.set_xlabel(u'Lunghezza [mm]')
+    ax2.set_ylabel(ur'Densità campionaria $[\frac{1}{mm}]$')
+
+    ticks = [13.70, 13.75, 13.80, 13.85, 13.90, 13.95]
+    ax2.set_xticks(ticks)
+
+    for label in ax2.get_xticklabels():
+        label.set_rotation(30)
+
+    ax2.grid(True)
+
+
+    # layout of second figure
+    f2.suptitle('Lunghezza dei cilindretti', y=0.95, fontsize=16)
+    f2.subplots_adjust(left=0.12, right=0.90, top=0.86, bottom=0.1, hspace=0.45)
+
+
+
+    # obvious isn't it?
+    plt.show()
--- esperienza_1/pendolo.py
+++ esperienza_1/pendolo.py
@@ -3,8 +3,14 @@
 from math import *
 import csv
 import itertools
-import matplotlib.pyplot as plt
-from matplotlib import rc
+
+mpl = False
+try:
+    import matplotlib.pyplot as plt
+    from matplotlib import rc
+    mpl = True
+except:
+    pass
 
 def mean(data):
     return sum(data) / len(data)
@@ -24,21 +30,27 @@ with open('pendolo/pendolo.csv') as csvfile:
 
         if n == 0:
             for i, name in enumerate(row):
-                header.append(name)
-                meas[name] = []
+                header.append(name.strip())
+                meas[name.strip()] = []
         else:
             for i, value in enumerate(row):
                 meas[header[i]].append(float(value) / 10.0)
-                tab[i].append(value.strip() + " s")
+                tab[i].append(value.strip())
 
-    tex += "\\begin{{tabular}} {{l || {}}}\n\t\\hline \\hline\n".format(" | ".join(["c" for i in range(l)]))
+    tex += "\\begin{{table}}\n\t\\begin{{tabular}} {{{}}}\n\t\t\\toprule\n".format(" | ".join(["c c c c"] * 3))
+    tex += "\t\t" + " & ".join(map(lambda x: "\\multicolumn{4}{c}{" + x + " s}", meas.keys())) + " \\\\\n\t\t\midrule\n"
 
-    for i in range(3):
-        tex += "\t" + header[i] + " & " + " & ".join(tab[i][:10]) + " \\\\ \cline{2-11} \n"
-        tex += "\t & " + " & ".join(tab[i][10:20]) + " \\\\ \hline \n"
+    for i in range(5):
+        t = []
+        for h in range(3):
+            t.append(" & ".join([x for n, x in enumerate(tab[h]) if ((n - i) % 5) == 0]))
 
-tex += '\t\\hline\n\\end{tabular}'
+        tex += "\t\t" + " & ".join(t) + " \\\\\n"
+
+tex += '\t\t\\bottomrule\n\t\\end{tabular}\n\\end{table}'
+
 print tex
+print
 
 xs = tuple(itertools.chain(*meas.values()))
 N = len(xs)
@@ -54,34 +66,35 @@ x_min = 1.4835
 x_max = 1.5135
 bins = [x_min + i * delta for i in range(int((x_max - x_min) / delta) + 1)]
 
-# istogramma
-n, r_bins, patches = plt.hist(xs, bins, (x_min, x_max),
-    normed=True, color=(0, 0.65, 1), alpha=0.7)
+if mpl:
+    # istogramma
+    n, r_bins, patches = plt.hist(xs, bins, (x_min, x_max),
+        normed=True, color=(0, 0.65, 1), alpha=0.7)
 
-plt.gca().set_ylim((0, 100))
-t = plt.title('Periodo del pendolo', fontsize=16)
-t.set_y(1.15)
-plt.suptitle('60 misure effettuate da 3 diversi sperimentatori', x=0.50, y=0.89)
-xlab = plt.xlabel('Periodo [s]', fontsize=14)
-plt.ylabel(u'Densità campionaria $[\\frac{1}{s}]$', fontsize=14)
-plt.grid(True)
-plt.xticks(rotation=30)
+    plt.gca().set_ylim((0, 100))
+    t = plt.title('Periodo del pendolo', fontsize=16)
+    t.set_y(1.15)
+    plt.suptitle('60 misure effettuate da 3 diversi sperimentatori', x=0.50, y=0.89)
+    xlab = plt.xlabel('Periodo [s]', fontsize=14)
+    plt.ylabel(u'Densità campionaria $[\\frac{1}{s}]$', fontsize=14)
+    plt.grid(True)
+    plt.xticks(rotation=30)
 
-plt.vlines(m, 0, 100, linestyles='dashed', linewidth=2, color=(0, 0, 1))
-plt.vlines(m - sigma - 0.00003, 0, 100, linestyles='dashed', linewidth=1, color=(0.55, 0.55, 0.55), zorder=-4)
-plt.vlines(m + sigma - 0.00003, 0, 100, linestyles='dashed', linewidth=1, color=(0.55, 0.55, 0.55), zorder=-4)
-plt.axvspan(m - sigma, m + sigma, color=(0.7, 0.7, 0.7), alpha=0.5, zorder=-5)
+    plt.vlines(m, 0, 100, linestyles='dashed', linewidth=2, color=(0, 0, 1))
+    plt.vlines(m - sigma - 0.00003, 0, 100, linestyles='dashed', linewidth=1, color=(0.55, 0.55, 0.55), zorder=-4)
+    plt.vlines(m + sigma - 0.00003, 0, 100, linestyles='dashed', linewidth=1, color=(0.55, 0.55, 0.55), zorder=-4)
+    plt.axvspan(m - sigma, m + sigma, color=(0.7, 0.7, 0.7), alpha=0.5, zorder=-5)
 
-prob = plt.twinx()
-prob.set_ylim((0, 100))
-prob.set_ylabel(u"Frequenza campionaria", fontsize=14)
-prob.set_yticklabels(["0", "0.06", "0.12", "0.18", "0.24", "0.30"])
+    prob = plt.twinx()
+    prob.set_ylim((0, 100))
+    prob.set_ylabel(u"Frequenza campionaria", fontsize=14)
+    prob.set_yticklabels(["0", "0.06", "0.12", "0.18", "0.24", "0.30"])
 
-plt.text(m - 0.0003, 102, "m")
-plt.text(m - sigma - 0.0013, 102, u"m - σ")
-plt.text(m + sigma - 0.0013, 102, u"m + σ")
+    plt.text(m - 0.0003, 102, "m")
+    plt.text(m - sigma - 0.0013, 102, u"m - σ")
+    plt.text(m + sigma - 0.0013, 102, u"m + σ")
 
-# make sure nothing goes outside the screen
-plt.subplots_adjust(left=0.1, right=0.9, top=0.8, bottom=0.15)
+    # make sure nothing goes outside the screen
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.8, bottom=0.15)
 
-plt.show()
+    plt.show()
