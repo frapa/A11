f229ae314e7ba0fa639ee55991929f6673901cc6
Francesco Pasa <francescopasa@gmail.com>
2013-05-14 21:49:58 +0200

    Commentati e corretti grafici

--- esperienza_3/lunghezza.py
+++ esperienza_3/lunghezza.py
@@ -72,22 +72,27 @@ ax.grid(True)
 
 ax.text(-0.03, -0.1, "0")
 
-ax.legend((dots, fit1), ("Periodi con incertezza", "Dipendenza non lineare"), 'upper left',
+ax.legend((dots, fit1), ("Dati sperimentali", "Dipendenza non lineare"), 'upper left',
         prop={'size': 12}, numpoints=1)
 
 f1.subplots_adjust(left=0.13, right=0.93, top=0.85, bottom=0.13)
 
 
-f2 = plt.figure(figsize=(8, 6))
-f2.suptitle("Dipendenza del periodo dalla lunghezza", y=0.93, fontsize=15)
+f2 = plt.figure(figsize=(8, 7))
+f2.suptitle("Logaritmi di lunghezza e periodo", y=0.97, fontsize=15)
 
 ax2 = f2.add_subplot(1, 1, 1)
 dots = ax2.errorbar(x=[log10(l) for l in L], y=[log10(t) for t in T],
     #yerr=dT, #xerr=sigma_res_p, 
     fmt='o', capsize=7)
 
-ax2.set_xlabel(u'Lunghezza del filo [m]', labelpad=12, fontsize=14)
-ax2.set_ylabel(u'Periodo [s]', labelpad=12, fontsize=14)
+ax2y = ax2.twinx()
+ax2x = ax2.twiny()
+
+ax2.set_xlabel(u'$X \;=\; \log_{10}(\ell)$', labelpad=12, fontsize=14)
+ax2.set_ylabel(u'$Y \;=\; \log_{10}(\,\mathcal{T}\,)$', labelpad=12, fontsize=14)
+ax2x.set_xlabel(u'Lunghezza del filo [m]', labelpad=12, fontsize=14)
+ax2y.set_ylabel(u'Periodo [s]', labelpad=12, fontsize=14)
 #ax2.set_xlabel(ur'$X \;=\; log(\ell)$', labelpad=12, fontsize=14)
 #ax2.set_ylabel(u'$Y \;=\; log(\mathcal{T})$', labelpad=12, fontsize=14)
 #ax.set_yscale('log')
@@ -96,20 +101,25 @@ ax2.set_ylabel(u'Periodo [s]', labelpad=12, fontsize=14)
 fit2 = ax2.errorbar(x=(-0.9, 0.1), y=(A - 0.9 * b, A + 0.1 * b))
 
 ax2.set_xlim((-0.9, 0.1))
-ax2.set_xticks([log10(l/100.0) for l in range(15, 125, 10)])
+ax2x.set_xlim((-0.9, 0.1))
+ax2.set_ylim((-0.2, 0.4))
+ax2y.set_ylim((-0.2, 0.4))
 
-ax2.set_xticklabels(['0.15', '0.25', '0.35', '0.45', '0.55', '0.65', '0.75', '0.85', '0.95', '1.05', '1.15'], rotation=45)
+ax2x.set_xticks([log10(l/100.0) for l in range(15, 125, 10)])
 
-ax2.set_yticks([log10(l/100.0) for l in range(75, 250, 25)])
-ax2.set_yticklabels([str(l/100.0) for l in range(75, 250, 25)])
-#ax.set_ylim((2.04, 2.07))
+ax2x.set_xticklabels(['0.15', '0.25', '0.35', '0.45', '0.55', '0.65', '0.75', '0.85', '0.95', '1.05', '1.15'], rotation=45)
+
+print [log10(l/100.0) for l in range(75, 250, 25)]
+ax2y.set_yticks([log10(l/100.0) for l in range(75, 250, 25)])
+ax2y.set_yticklabels([str(l/100.0) for l in range(75, 250, 25)])
+
 ax2.grid(True)
 
 #ax2.text(-0.03, -0.1, "0")
 
-ax2.legend((dots, fit2), ("Periodi con incertezza", "Dipendenza lineare"), 'upper left',
+ax2.legend((dots, fit2), ("Dati sperimentali", "Dipendenza lineare $Y = A + bX$"), 'upper left',
         prop={'size': 12}, numpoints=1)
 
-f2.subplots_adjust(left=0.13, right=0.93, top=0.88, bottom=0.16)
+f2.subplots_adjust(left=0.13, right=0.87, top=0.80, bottom=0.13)
 
 plt.show()
